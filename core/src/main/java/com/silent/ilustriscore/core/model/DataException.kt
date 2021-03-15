79dc6bc7b93675b9f85b0f12ce18feec35582a58
CaioProgramming <caioalvesferreiracontato@gmail.com>
2021-03-14 22:31:14 -0300

    exception attributes are now visible

--- core/src/main/java/com/silent/ilustriscore/core/model/DataException.kt
+++ core/src/main/java/com/silent/ilustriscore/core/model/DataException.kt
@@ -2,7 +2,7 @@ package com.silent.ilustriscore.core.model
 
 
 enum class ErrorType { NOT_FOUND, UNKNOWN, DISCONNECTED, UPDATE, DELETE }
-class DataException(message: String? = "Unknown error", code: ErrorType = ErrorType.UNKNOWN) : Exception() {
+class DataException(override val message: String? = "Unknown error", val code: ErrorType = ErrorType.UNKNOWN) : Exception() {
 
     companion object {
         fun fromException(e: Exception): DataException {
