f5142743fc1b3a78dbca03450f423d68b7ffd440
CaioProgramming <caioalvesferreiracontato@gmail.com>
2021-12-15 13:25:17 -0300

    reverting ViewModelContract.kt changes

--- core/src/main/java/com/silent/ilustriscore/core/contract/ViewModelContract.kt
+++ core/src/main/java/com/silent/ilustriscore/core/contract/ViewModelContract.kt
@@ -1,5 +1,7 @@
 package com.silent.ilustriscore.core.contract
 
-import com.silent.ilustriscore.core.bean.BaseBean
+import com.silent.ilustriscore.core.model.BaseService
 
-interface ViewModelContract<T> where T : BaseBean
+interface ViewModelContract {
+    val service: BaseService
+}
