0636c1bfddb84064e5846d965ebeb27a66b258d7
CaioProgramming <caioalvesferreiracontato@gmail.com>
2022-05-18 19:44:31 -0300

    added new format to date extension

--- core/src/main/java/com/silent/ilustriscore/core/utilities/DateExtensions.kt
+++ core/src/main/java/com/silent/ilustriscore/core/utilities/DateExtensions.kt
@@ -3,13 +3,14 @@ package com.silent.ilustriscore.core.utilities
 import java.text.SimpleDateFormat
 import java.util.*
 
-enum class DateFormats(val format: String) {
+enum class DateFormats(val formatted: String) {
     DD_OF_MM_FROM_YYYY("dd 'de' MMMM 'de' yyyy"),
     DD_MM_YYY("DD/MM/YYY"),
     MM_DD_YYY("MM/DD/YY"),
     M_D_Y("Month D, Yr"),
     EE_D_MMM_YYY("EEE, MMM d, ''yy"),
-    EE_DD_MMM_YYY_HH_MM("EEE, d MMM yyyy HH:mm")
+    EE_DD_MMM_YYY_HH_MM("EEE, d MMM yyyy HH:mm"),
+    DD_MM_HH("DD.MM - HH")
 }
 
 
@@ -18,5 +19,13 @@ fun Date.formatDate(format: String? = null): String {
         format,
         Locale.getDefault()
     ) else SimpleDateFormat.getDateInstance()
+    return fmt.format(this)
+}
+
+fun Date.format(format: DateFormats) : String {
+    val fmt =  SimpleDateFormat(
+        format.formatted,
+        Locale.getDefault()
+    )
     return fmt.format(this)
 }
